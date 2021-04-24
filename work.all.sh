71332475a6f2607aa3e707581240c923abdc5685
Oscar Koeroo <okoeroo@gmail.com>
2021-04-24 20:00:24 +0200

    all commits

--- work.all.sh
+++ work.all.sh
@@ -1,15 +1,6 @@
-./transip-cmd.py --cmd add      --domain koeroo.com --name test1 --expire 777 --type TXT --content "test 1 2 3" --login okoeroo --private-key keys/gamora.api.key
-echo "Sleep 15"
-sleep 15
-dig TXT test1.koeroo.com  @ns0.transip.net
-echo
-./transip-cmd.py --cmd remove   --domain koeroo.com --name test1 --expire 777 --type TXT --content "test 1 2 3" --login okoeroo --private-key keys/gamora.api.key
-echo "Sleep 15"
-sleep 15
-dig TXT test1.koeroo.com  @ns0.transip.net
-echo
+#!/bin/bash -x
 
-exit 0
+./transip-cmd.py --cmd add      --domain koeroo.com --name test1 --expire 777 --type TXT --content "test 1 2 3" --login okoeroo --private-key keys/gamora.api.key
 
 echo
 ./transip-cmd.py --cmd danetlsa --domain koeroo.net --protocol imap --fqdn  smtp.koeroo.net --port 143 --login okoeroo --private-key keys/gamora.api.key
@@ -17,4 +8,15 @@ echo
 ./transip-cmd.py --cmd danetlsa --domain koeroo.net --protocol tls  --fqdn  smtp.koeroo.net --port 465 --login okoeroo --private-key keys/gamora.api.key
 ./transip-cmd.py --cmd danetlsa --domain koeroo.net --protocol tls  --fqdn  smtp.koeroo.net --port 993 --login okoeroo --private-key keys/gamora.api.key
 ./transip-cmd.py --cmd danetlsa --domain koeroo.net --protocol tls  --fqdn cloud.koeroo.net --port 443 --login okoeroo --private-key keys/gamora.api.key
+
+dig TXT test1.koeroo.com  @ns0.transip.net
+echo "Sleeping 30"
+sleep 30
+dig TXT test1.koeroo.com  @ns0.transip.net
+echo
+#./transip-cmd.py --cmd remove   --domain koeroo.com --name test1 --expire 777 --type TXT --content "test 1 2 3" --login okoeroo --private-key keys/gamora.api.key
+echo "Sleeping 15"
+sleep 15
+dig TXT test1.koeroo.com  @ns0.transip.net
+echo
 
